792ac7e276f90bd266a413b2616ed1b81f11faf2
frederikprijck <frederik.prijck@auth0.com>
2021-05-25 13:57:06 +0200

    Update to webpack 5 to fix vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const BabiliPlugin = require('babili-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -8,15 +8,26 @@ module.exports = {
   },
   output: {
     filename: '[name]',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    environment: {
+      arrowFunction: false,
+      bigIntLiteral: false,
+      const: false,
+      destructuring: false,
+      dynamicImport: false,
+      forOf: false,
+      module: false,
+    }
   },
   externals: {
     angular: 'angular',
     'auth0-js': 'auth0'
   },
-  plugins: [
-    new BabiliPlugin({}, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
       test: /\.min\.js$/
-    })
-  ]
+    })],
+  }
 };
